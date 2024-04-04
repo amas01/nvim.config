014cf5eb7524c2f93425714c4b6b0bbb2b6bf5df
Ammar Asaria <ammar.asaria@hcl.com>
2024-04-04 10:03:46 +0100

    clean up

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -44,35 +44,29 @@ return {
       --  - ]i moves to bottom of indentation
       require('mini.indentscope').setup()
 
+      -- mini bufremove - delete current buffer
+      require('mini.bufremove').setup()
+      vim.keymap.set('n', '<leader>bd', function()
+        local bd = require('mini.bufremove').delete
+        if vim.bo.modified then
+          local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
+          if choice == 1 then -- Yes
+            vim.cmd.write()
+            bd(0)
+          elseif choice == 2 then -- No
+            bd(0, true)
+          end
+        else
+          bd(0)
+        end
+      end, { desc = 'delete buffer' })
+      vim.keymap.set('n', '<leader>bD', function()
+        require('mini.bufremove').delete(0, true)
+      end, { desc = 'Delete Buffer (Force)' })
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
-  },
-  {
-    'echasnovski/mini.bufremove',
-
-    keys = {
-      {
-        '<leader>bd',
-        function()
-          local bd = require('mini.bufremove').delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = 'Delete Buffer',
-      },
-      -- stylua: ignore
-      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
