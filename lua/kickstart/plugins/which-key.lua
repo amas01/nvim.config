8f0ac2fe055e8fc23db98949eecf509d82fef7cb
Ammar Asaria <theone786@gmail.com>
2024-04-05 05:46:40 +0100

    added dressing plugin

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -27,8 +27,11 @@ return {
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
+ {
+  'stevearc/dressing.nvim',
+  opts = {},
+}       ['<leader>g'] = { name = '[g]it', _ = 'which_key_ignore' },
       }
     end,
   },
